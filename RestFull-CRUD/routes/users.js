49cad44350ef17ba50fc158c3f621aa2a10a18c8
gabrielemuci98 <muci.gabriele@iisgalvanimi.edu.it>
2020-01-13 10:03:45 +0000

    esercizio finito

---
+++ RestFull-CRUD/routes/users.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+const sql = require('mssql')
+const config = {
+  user: '4DD_09',  //Vostro user name
+  password: 'xxx123##', //Vostra password
+  server: "213.140.22.237",  //Stringa di connessione
+  database: '4DD_09', //(Nome del DB)
+}
+
+/* GET users listing. */
+router.get('/attori', function(req, res, next) {
+  sql.connect(config, err => {
+    if(err) console.log(err);  // ... error check
+    
+    // Query
+    let sqlRequest = new sql.Request();  //Oggetto che serve a creare le query
+    sqlRequest.query('select * from DATIIMPIEGATO', (err, result) => {
+        if (err) console.log(err); // ... error checks
+        res.send(result);  //Invio il risultato
+    });
+  });
+});
+
+router.get('/search/:name', function(req, res, next) {
+  sql.connect(config, err => {
+    // ... error check
+    if(err) console.log(err);
+    // Query
+    let sqlRequest = new sql.Request();
+    sqlRequest.query(`select * from DATIIMPIEGATO where Cognome = '${req.params.cognome}'`, (err, result) => {
+        // ... error checks
+        if (err) console.log(err);
+
+        res.send(result);
+    });
+  });
+});
+
+module.exports = router;
